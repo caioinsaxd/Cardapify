0bca4f077f73811f4120b24b1a60b40835bd6e6d
caioinsaxd <caiosantanaparize2015@gmail.com>
2026-03-06 23:37:09 -0300

    refactor: update do schema + integrando modulos CRUD

--- cardapify-backend/src/app.module.ts
+++ cardapify-backend/src/app.module.ts
@@ -5,6 +5,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { SettingsModule } from './settings/settings.module';
+import { CategoryModule } from './category/category.module';
+import { ProductModule } from './product/product.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
   imports: [
@@ -28,6 +32,10 @@ import { AuthModule } from './auth/auth.module';
     ]),
     PrismaModule,
     AuthModule,
+    SettingsModule,
+    CategoryModule,
+    ProductModule,
+    OrderModule,
   ],
   controllers: [AppController],
   providers: [AppService],
