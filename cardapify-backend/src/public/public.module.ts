0482d355101be2cd6f77070451416f9fdee46ae9
caioinsaxd <caiosantanaparize2015@gmail.com>
2026-03-24 20:03:28 -0300

    fix: arrumando erro de import dos modules

--- cardapify-backend/src/public/public.module.ts
+++ cardapify-backend/src/public/public.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PublicController } from './public.controller';
 import { PublicService } from './public.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [PublicController],
   providers: [PublicService],
 })
