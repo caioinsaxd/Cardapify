f98c9b96ad7f88179440e724a08bc6f5af783052
caioinsaxd <caiosantanaparize2015@gmail.com>
2026-03-27 23:59:07 -0300

    feat: add toast provider

--- cardapify-frontend/components/providers.tsx
+++ cardapify-frontend/components/providers.tsx
@@ -2,11 +2,14 @@
 
 import { QueryProvider } from '@/contexts/QueryProvider';
 import { AuthProvider } from '@/contexts/AuthContext';
+import { ToastProvider } from '@/contexts/ToastContext';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryProvider>
-      <AuthProvider>{children}</AuthProvider>
+      <AuthProvider>
+        <ToastProvider>{children}</ToastProvider>
+      </AuthProvider>
     </QueryProvider>
   );
 }
